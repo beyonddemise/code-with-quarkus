b52587aaf52859e555309c45fbafc9ca90afd0d8
Stephan H. Wissel <stw@linux.com>
2024-11-25 21:09:41 +0800

    added reproducer for json-schema

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,5 +1,10 @@
 package org.acme;
 
+import io.quarkus.vertx.web.Route;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.openapi.contract.OpenAPIContract;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -26,6 +31,29 @@ public class GreetingResource {
             return new String(is.readAllBytes(), java.nio.charset.StandardCharsets.UTF_8);
         } catch (java.io.IOException e) {
             return "{\"error\": \"" + e.getMessage() + "\"}";
+        }
+
+    }
+
+    @Route(path = "/hello/pet", methods = Route.HttpMethod.GET)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void pet(RoutingContext ctx) {
+        try {
+            // Petsstore is a OpenAPI 3.1 JSON example
+            java.io.InputStream is = getClass().getClassLoader().getResourceAsStream("petstore-v3.1.json");
+            if (is == null) {
+                ctx.json("{\"error\": \"Resource not found\"}");
+            }
+            // Now read the file into a String and turn it into an OpenAPI model
+            // parsing the model will trigger the read operation on json-schema.org/**
+            String textContract = new String(is.readAllBytes(), java.nio.charset.StandardCharsets.UTF_8);
+            JsonObject jsonContract = new JsonObject(textContract);
+            OpenAPIContract.from(ctx.vertx(), jsonContract)
+                    .onFailure(ctx::fail)
+                    .onSuccess(oai -> ctx
+                            .json("Successful loaded OpenAPI contract petstore-v3.1.json:" + oai.getVersion()));
+        } catch (java.io.IOException e) {
+            ctx.json("{\"error\": \"" + e.getMessage() + "\"}");
         }
 
     }
